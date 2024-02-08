d1ddb19187e5db3876f1a177fe404ebc4b39793c
TristanWolfram <tris.wolfram@googlemail.com>
2024-02-08 20:41:17 +0100

    Update binary files

--- Project_1/GA_feature_selection.py
+++ Project_1/GA_feature_selection.py
@@ -1,20 +1,30 @@
+import LinReg as lr
 import numpy as np
 import pandas as pd
-import LinReg as lr
 
-class GA_fe_sel:
+
+class GA_feature_selection:
     def __init__(self):
         self.myRNG = np.random.default_rng()
         self.regressor = lr.LinReg()
 
         print("Initialized GA for feature selection task!")
 
-    def run(self, data_str, generation_num, population_size, crossover_rate, mutation_rate):
+    def run(
+        self,
+        data_str,
+        generation_num,
+        population_size,
+        crossover_rate,
+        mutation_rate,
+        crowding=False,
+    ):
 
-        data = pd.read_csv(data_str)
+        data = pd.read_csv(data_str, header=None)
         num_features = data.shape[1] - 1
-
-        print(f"started GA with data: {data.shape}, num_features: {num_features}, population_size: {population_size}, generation number: {generation_num}, crossover rate: {crossover_rate}, mutation rate: {mutation_rate}")
+        print(
+            f"started GA with data shape: {data.shape}, num_features: {num_features}, population_size: {population_size}, generation number: {generation_num}, crossover rate: {crossover_rate}, mutation rate: {mutation_rate}"
+        )
 
         # Initialize population
         population = self.generate_initial_population(population_size, num_features)
@@ -22,31 +32,59 @@ class GA_fe_sel:
 
         generations = []
         fitness_per_generation = []
+        average_fitness = []
+        min_fitness = []
+        sum_fitness = []
 
         for i in range(generation_num):
-            i += 1
             print(f"Generation: {i}")
-            population_new, fitness_new = self.generate_generation(data, population, crossover_rate, mutation_rate)
+            population_new, fitness_new = self.generate_generation(
+                data, population, crossover_rate, mutation_rate, crowding
+            )
             print(f"Population shape: {population_new.shape}")
             print(f"Fitness: {fitness_new}")
             population = population_new
 
             generations.append(population_new)
             fitness_per_generation.append(fitness_new)
+            average_fitness.append(np.sum(fitness_new) / len(fitness_new))
+            min_fitness.append(np.min(fitness_new))
+            sum_fitness.append(np.sum(fitness_new))
 
+        print(
+            "GA finished with :",
+            np.sum(np.array(fitness_per_generation[-1]))
+            / len(np.array(fitness_per_generation[-1])),
+        )
+        return (
+            generations,
+            fitness_per_generation,
+            average_fitness,
+            min_fitness,
+            sum_fitness,
+        )
 
-        print("GA finished with :", np.sum(np.array(fitness_per_generation[-1])) / len(np.array(fitness_per_generation[-1])))
-        return generations, fitness_per_generation
+    def generate_generation(
+        self, data, population, crossover_rate, mutation_rate, crowding
+    ):
 
-    def generate_generation(self, data, population, crossover_rate, mutation_rate):
+        if crowding:
+            pop_new, fitness_new = self.deterministic_crowding(
+                data, population, crossover_rate, mutation_rate
+            )
+        else:
+            pop_new, fitness_new = self.generation_replacement(
+                data, population, crossover_rate, mutation_rate
+            )
 
-        # TODO: Implement further, not working now!!
+        return pop_new, fitness_new
 
+    def generation_replacement(self, data, population, crossover_rate, mutation_rate):
         pop_new = []
         j = 1
         while j <= population.shape[0]:
 
-            parents = self.select_best(data, population, 2)
+            parents = self.roulette_wheel_selection(data, population, 2)
 
             offspring = self.generate_offspring(parents[0], parents[1], crossover_rate)
 
@@ -55,27 +93,98 @@ class GA_fe_sel:
                 pop_new.append(offspring[i])
 
             j += 2
-        
+
         pop_new = np.array(pop_new)
-        fitness_new = self.greate_fitness_scores(data, pop_new)
+        fitness_new = self.create_fitness_scores(data, pop_new)
+
+        return pop_new, fitness_new
+
+    def deterministic_crowding(self, data, population, crossover_rate, mutation_rate):
+
+        pop_new = population.copy()
+        j = 1
+
+        while j <= population.shape[0]:
+
+            parents = self.roulette_wheel_selection(data, pop_new, 2)
+
+            offspring = self.generate_offspring(parents[0], parents[1], crossover_rate)
+
+            for k in range(len(offspring)):
+                offspring[k] = self.mutate_gene(offspring[k], mutation_rate)
+
+            fit_p1 = self.get_fitness_of_bitstring(data, parents[0])
+            fit_p2 = self.get_fitness_of_bitstring(data, parents[1])
+
+            fit_o1 = self.get_fitness_of_bitstring(data, offspring[0])
+            fit_o2 = self.get_fitness_of_bitstring(data, offspring[1])
+
+            prob_replacement = 0.5
+            if fit_o1 < fit_p1:
+                prob_replacement = 1
+            elif fit_o1 == fit_p1:
+                prob_replacement = 0.5
+            else:
+                prob_replacement = 0
+
+            if self.myRNG.random() < prob_replacement:
+                pop_new = np.delete(
+                    pop_new,
+                    np.where(np.all(pop_new == parents[0], axis=1))[0][0],
+                    axis=0,
+                )
+                pop_new = np.append(pop_new, [offspring[0]], axis=0)
+
+            if fit_o2 < fit_p2:
+                prob_replacement = 1
+            elif fit_o2 == fit_p2:
+                prob_replacement = 0.5
+            else:
+                prob_replacement = 0
+
+            if self.myRNG.random() < prob_replacement:
+                if np.all(pop_new == parents[1], axis=1).any():
+                    pop_new = np.delete(
+                        pop_new,
+                        np.where(np.all(pop_new == parents[1], axis=1))[0][0],
+                        axis=0,
+                    )
+                    pop_new = np.append(pop_new, [offspring[1]], axis=0)
+
+            j += 2
+
+        fitness_new = self.create_fitness_scores(data, pop_new)
 
         return pop_new, fitness_new
 
     def generate_initial_population(self, population_size, num_features):
-        return self.myRNG.integers(0, 1, size=(population_size, num_features), endpoint=True)
-    
-    def greate_fitness_scores(self, data, population):
+        individuals = self.myRNG.integers(
+            0, 1, size=(population_size, num_features), endpoint=True
+        )
+        ones_column = np.ones((population_size, 1), dtype=int)
+        individuals = np.hstack((individuals, ones_column))
+        return individuals
+
+    def create_fitness_scores(self, data, population):
         fitness_scores = []
         for i in range(population.shape[0]):
-            X = self.regressor.get_columns(data.values, population[i])
-            fitness = self.regressor.get_fitness(X[:,:-1], X[:,-1])
+            fitness = self.get_fitness_of_bitstring(data, population[i])
             fitness_scores.append(fitness)
         return np.array(fitness_scores)
-    
+
+    def get_fitness_of_bitstring(self, data, bit_string):
+        X = self.regressor.get_columns(data.values, bit_string)
+        fitness = self.regressor.get_fitness(X[:, :-1], X[:, -1], 20)
+
+        return fitness
+
     def roulette_wheel_selection(self, data, population, num_parents):
-        fitness_scores = self.greate_fitness_scores(data, population) 
-        sum_fitness = np.sum(fitness_scores)
-        selection_prob = fitness_scores / sum_fitness
+        fitness_scores = self.create_fitness_scores(data, population)
+
+        fitness_inverse = 1 / fitness_scores
+
+        sum_fitness = np.sum(fitness_inverse)
+        selection_prob = fitness_inverse / sum_fitness
         cum_prob = np.cumsum(selection_prob)
 
         parents = []
@@ -83,23 +192,27 @@ class GA_fe_sel:
             r = self.myRNG.random()
             index = np.where(cum_prob > r)[0][0]
             parents.append(population[index])
-        
+
         return np.array(parents)
-    
-    def select_best(self, data, population, num_parents):
-        fitness_scores = self.greate_fitness_scores(data, population)
-        best = np.argsort(fitness_scores)[:-num_parents]
+
+    def select_best(self, population, num_parents):
+        fitness_scores = self.create_fitness_scores(population)
+        best = np.argsort(fitness_scores)[-num_parents:]
         return population[best]
-    
+
     def generate_offspring(self, parent1, parent2, crossover_rate):
         if self.myRNG.random() < crossover_rate:
             crossover_point = self.myRNG.integers(1, len(parent1))
-            offspring1 = np.concatenate([parent1[:crossover_point], parent2[crossover_point:]])
-            offspring2 = np.concatenate([parent2[:crossover_point], parent1[crossover_point:]])
+            offspring1 = np.concatenate(
+                [parent1[:crossover_point], parent2[crossover_point:]]
+            )
+            offspring2 = np.concatenate(
+                [parent2[:crossover_point], parent1[crossover_point:]]
+            )
             return np.array([offspring1, offspring2])
         else:
             return np.array([parent1, parent2])
-    
+
     def mutate_gene(self, bit_string, mutation_rate):
         for i in range(len(bit_string)):
             if self.myRNG.random() < mutation_rate:
--- Project_1/GA_sine.py
+++ Project_1/GA_sine.py
@@ -1,6 +1,9 @@
+import copy
+
+import LinReg as lr
 import numpy as np
 import pandas as pd
-import LinReg as lr
+
 
 class GA_sine:
     def __init__(self):
@@ -8,10 +11,20 @@ class GA_sine:
 
         print("Initialized GA for sine function task!")
 
-    def run(self, num_features, generation_num, population_size, crossover_rate, mutation_rate):
+    def run(
+        self,
+        num_features,
+        generation_num,
+        population_size,
+        crossover_rate,
+        mutation_rate,
+        limitation=None,
+        crowding=False,
+    ):
 
-
-        print(f"started GA with num_features: {num_features}, population_size: {population_size}, generation number: {generation_num}, crossover rate: {crossover_rate}, mutation rate: {mutation_rate}")
+        print(
+            f"started GA with num_features: {num_features}, population_size: {population_size}, generation number: {generation_num}, crossover rate: {crossover_rate}, mutation rate: {mutation_rate}"
+        )
 
         # Initialize population
         population = self.generate_initial_population(population_size, num_features)
@@ -19,31 +32,61 @@ class GA_sine:
 
         generations = []
         fitness_per_generation = []
+        average_fitness = []
+        max_fitness = []
+        sum_fitness = []
 
         for i in range(generation_num):
-            i += 1
             print(f"Generation: {i}")
-            population_new, fitness_new = self.generate_generation(population, crossover_rate, mutation_rate)
+            population_new, fitness_new = self.generate_generation(
+                population, crossover_rate, mutation_rate, limitation, crowding
+            )
             print(f"Population shape: {population_new.shape}")
             print(f"Fitness: {fitness_new}")
             population = population_new
 
             generations.append(population_new)
             fitness_per_generation.append(fitness_new)
+            average_fitness.append(np.sum(fitness_new) / len(fitness_new))
+            max_fitness.append(np.max(fitness_new))
+            sum_fitness.append(np.sum(fitness_new))
 
+        print(
+            "GA finished with :",
+            np.sum(np.array(fitness_per_generation[-1]))
+            / len(np.array(fitness_per_generation[-1])),
+        )
+        return (
+            generations,
+            fitness_per_generation,
+            average_fitness,
+            max_fitness,
+            sum_fitness,
+        )
 
-        print("GA finished with :", np.sum(np.array(fitness_per_generation[-1])) / len(np.array(fitness_per_generation[-1])))
-        return generations, fitness_per_generation
+    def generate_generation(
+        self, population, crossover_rate, mutation_rate, limitation, crowding
+    ):
 
-    def generate_generation(self, population, crossover_rate, mutation_rate):
+        if crowding:
+            pop_new, fitness_new = self.deterministic_crowding(
+                population, crossover_rate, mutation_rate, limitation
+            )
+        else:
+            pop_new, fitness_new = self.generation_replacement(
+                population, crossover_rate, mutation_rate, limitation
+            )
 
-        # TODO: Implement further, not working now!!
+        return pop_new, fitness_new
 
+    def generation_replacement(
+        self, population, crossover_rate, mutation_rate, limitation
+    ):
         pop_new = []
         j = 1
         while j <= population.shape[0]:
 
-            parents = self.select_best(population, 2)
+            parents = self.roulette_wheel_selection(population, 2, limitation)
 
             offspring = self.generate_offspring(parents[0], parents[1], crossover_rate)
 
@@ -52,35 +95,104 @@ class GA_sine:
                 pop_new.append(offspring[i])
 
             j += 2
-        
+
         pop_new = np.array(pop_new)
-        fitness_new = self.greate_fitness_scores(pop_new)
+        fitness_new = self.create_fitness_scores(pop_new, limitation)
+
+        return pop_new, fitness_new
+
+    def deterministic_crowding(
+        self, population, crossover_rate, mutation_rate, limitation
+    ):
+
+        pop_new = population.copy()
+        j = 1
+
+        while j <= population.shape[0]:
+
+            parents = self.roulette_wheel_selection(pop_new, 2, limitation)
+
+            offspring = self.generate_offspring(parents[0], parents[1], crossover_rate)
+            print(offspring)
+
+            for k in range(len(offspring)):
+                offspring[k] = self.mutate_gene(offspring[k], mutation_rate)
+
+            fit_p1 = self.get_fitness_of_bitstring(parents[0], limitation)
+            fit_p2 = self.get_fitness_of_bitstring(parents[1], limitation)
+
+            fit_o1 = self.get_fitness_of_bitstring(offspring[0], limitation)
+            fit_o2 = self.get_fitness_of_bitstring(offspring[1], limitation)
+
+            prob_replacement = 0.5
+            if fit_o1 > fit_p1:
+                prob_replacement = 1
+            elif fit_o1 == fit_p1:
+                prob_replacement = 0.5
+            else:
+                prob_replacement = 0
+
+            if self.myRNG.random() < prob_replacement:
+                pop_new = np.delete(
+                    pop_new,
+                    np.where(np.all(pop_new == parents[0], axis=1))[0][0],
+                    axis=0,
+                )
+                pop_new = np.append(pop_new, [offspring[0]], axis=0)
+
+            if fit_o2 > fit_p2:
+                prob_replacement = 1
+            elif fit_o2 == fit_p2:
+                prob_replacement = 0.5
+            else:
+                prob_replacement = 0
+
+            if self.myRNG.random() < prob_replacement:
+                if np.all(pop_new == parents[1], axis=1).any():
+                    pop_new = np.delete(
+                        pop_new,
+                        np.where(np.all(pop_new == parents[1], axis=1))[0][0],
+                        axis=0,
+                    )
+                    pop_new = np.append(pop_new, [offspring[1]], axis=0)
+
+            j += 2
+
+        fitness_new = self.create_fitness_scores(pop_new, limitation)
 
         return pop_new, fitness_new
 
     def generate_initial_population(self, population_size, num_features):
-        return self.myRNG.integers(0, 1, size=(population_size, num_features), endpoint=True)
-    
-    def greate_fitness_scores(self, population):
+        return self.myRNG.integers(
+            0, 1, size=(population_size, num_features), endpoint=True
+        )
+
+    def create_fitness_scores(self, population, limitation):
         fitness_scores = []
         for i in range(population.shape[0]):
-            int_value = int(''.join(map(str, population[i])), 2)
-            scaled_value = int_value * (128 / 2**population.shape[1])
 
-            fitness = np.sin(scaled_value)
+            fitness = self.get_fitness_of_bitstring(population[i], limitation)
+
             fitness_scores.append(fitness)
         return np.array(fitness_scores)
 
+    def get_fitness_of_bitstring(self, bit_string, limitation):
+        int_value = int("".join(map(str, bit_string)), 2)
+        scaled_value = int_value * (128 / 2 ** len(bit_string))
 
-        # fitness_scores = []
-        # for i in range(population.shape[0]):
-        #     X = self.regressor.get_columns(data.values, population[i])
-        #     fitness = self.regressor.get_fitness(X[:,:-1], X[:,-1])
-        #     fitness_scores.append((fitness, i))
-        # return fitness_scores
-    
-    def roulette_wheel_selection(self, population, num_parents):
-        fitness_scores = self.greate_fitness_scores(population) 
+        distance = 0
+        if limitation is not None:
+            if scaled_value < limitation[0]:
+                distance = limitation[0] - scaled_value
+            elif scaled_value > limitation[1]:
+                distance = scaled_value - limitation[1]
+
+        fitness = np.sin(scaled_value) + 1 - (distance / 100)
+
+        return fitness
+
+    def roulette_wheel_selection(self, population, num_parents, limitation):
+        fitness_scores = self.create_fitness_scores(population, limitation)
         sum_fitness = np.sum(fitness_scores)
         selection_prob = fitness_scores / sum_fitness
         cum_prob = np.cumsum(selection_prob)
@@ -90,23 +202,27 @@ class GA_sine:
             r = self.myRNG.random()
             index = np.where(cum_prob > r)[0][0]
             parents.append(population[index])
-        
+
         return np.array(parents)
-    
+
     def select_best(self, population, num_parents):
-        fitness_scores = self.greate_fitness_scores(population)
+        fitness_scores = self.create_fitness_scores(population)
         best = np.argsort(fitness_scores)[-num_parents:]
         return population[best]
-    
+
     def generate_offspring(self, parent1, parent2, crossover_rate):
         if self.myRNG.random() < crossover_rate:
             crossover_point = self.myRNG.integers(1, len(parent1))
-            offspring1 = np.concatenate([parent1[:crossover_point], parent2[crossover_point:]])
-            offspring2 = np.concatenate([parent2[:crossover_point], parent1[crossover_point:]])
+            offspring1 = np.concatenate(
+                [parent1[:crossover_point], parent2[crossover_point:]]
+            )
+            offspring2 = np.concatenate(
+                [parent2[:crossover_point], parent1[crossover_point:]]
+            )
             return np.array([offspring1, offspring2])
         else:
             return np.array([parent1, parent2])
-    
+
     def mutate_gene(self, bit_string, mutation_rate):
         for i in range(len(bit_string)):
             if self.myRNG.random() < mutation_rate:
